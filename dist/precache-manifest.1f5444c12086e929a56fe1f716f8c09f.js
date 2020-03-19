b1bc4b62ddb749b8a4d84683eac9a41dde50adc7
imjohnlouie04 <imjohnlouie@gmail.com>
2020-03-19 08:12:58 +0800

    Dist folder

---
+++ dist/precache-manifest.1f5444c12086e929a56fe1f716f8c09f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606424f8738191e65359",
+    "url": "/imjohnlouie04.github.io/css/app.17d0086d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/imjohnlouie04.github.io/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "deeede1d3e312cff38affc8071751930",
+    "url": "/imjohnlouie04.github.io/index.html"
+  },
+  {
+    "revision": "26ffe399451be9845c9c",
+    "url": "/imjohnlouie04.github.io/js/about.8af0243a.js"
+  },
+  {
+    "revision": "606424f8738191e65359",
+    "url": "/imjohnlouie04.github.io/js/app.062f5787.js"
+  },
+  {
+    "revision": "d237d81727e15d1b6640",
+    "url": "/imjohnlouie04.github.io/js/chunk-vendors.4d8491ab.js"
+  },
+  {
+    "revision": "524026b72796bb8fa0e5b02a24a09049",
+    "url": "/imjohnlouie04.github.io/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/imjohnlouie04.github.io/robots.txt"
+  }
+]);
