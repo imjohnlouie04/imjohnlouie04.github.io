4dd38164d7dedfe9dbc0e994728d358c89bebd61
imjohnlouie04 <imjohnlouie@gmail.com>
2020-03-20 12:14:21 +0800

    deploy

---
+++ precache-manifest.25f27178eefe2f958b1c2965a34be140.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6715fd93c63e11396b74",
+    "url": "/css/app.d5e18deb.css"
+  },
+  {
+    "revision": "0c622323e1c0603d2d66bbf9fa726c84",
+    "url": "/img/me.0c622323.jpg"
+  },
+  {
+    "revision": "341af579fb4e17573feba0f570f9b5cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5153143b3bcf311f9aa8",
+    "url": "/js/about.76cd06fd.js"
+  },
+  {
+    "revision": "6715fd93c63e11396b74",
+    "url": "/js/app.42e98206.js"
+  },
+  {
+    "revision": "1dedf035600d4e547261",
+    "url": "/js/chunk-vendors.e615d323.js"
+  },
+  {
+    "revision": "524026b72796bb8fa0e5b02a24a09049",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
